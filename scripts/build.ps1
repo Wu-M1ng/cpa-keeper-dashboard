906362a016ac19ef53f930556002c41fc17533f3
Wu-M1ng <126851374+Wu-M1ng@users.noreply.github.com>
2026-08-27 15:49:04 +0800

    Add files via upload

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,6 +1,6 @@
 [CmdletBinding()]
 param(
-  [string]$Version = "1.4.2",
+  [string]$Version = "1.4.3",
   [string]$GoOS = $env:GOOS,
   [string]$GoArch = $env:GOARCH,
   [string]$OutputRoot = "release"
